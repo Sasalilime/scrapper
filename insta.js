05549a0d9c79f43bb11aa55b7f06e4520610b9f8
salim <salimtison@gmail.Com>
2021-05-11 15:53:06 +0200

    first commit

--- insta.js
+++ insta.js
@@ -6,11 +6,36 @@ const url = 'https://www.instagram.com/';
 
 
 (async () => {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: "networkidle2"});
 
-    // await browser.close();
+    //cookie
+    await page.click('._1XyCr > button');
+
+    //login
+    await page.type('[name=username]', 'salimtison@gmail.com', {
+        delay: 100
+    });
+    await page.type('[name=password]', 'kaka12', {
+        delay: 100
+    });
+    await page.click('button[type=submit]');
+
+    //autoconnect
+    // await page.waitForSelector('._1Xy445- > button', {visible: true})
+    // await page.click('._1Xy445- > button');
+
+    //notifications (ACTIVED)
+    // await page.waitForSelector('.mt3GC > button', {visible: true})
+    // await page.click('.mt3GC > button');
+
+    //notifications (ACTIVED)
+     await page.waitForSelector('.mt3GC > button:nth-child(2)', {visible: true})
+     await page.click('.mt3GC > button');
+
+
+   // await browser.close();
 
 })();
 
